068243cb9b94863aba40945b887fb88fd56ace0e
DmitryiKD <dimka_2284@mail.ru>
2022-12-12 19:39:24 +0300

    Added task 41, 43

---
+++ task_41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+
+string NumberEntry(string str)                                                              // Метод для ввода чисел в виде строки
+{
+    Console.Write(str);
+    string number = Console.ReadLine();
+    return number;
+}
+
+void CountNumbersOverZero(string str)                                                      // Метод для подсчета чисел больше ноля в строке 
+{
+    string sum = string.Empty;
+    int number = 0;
+    int count = 0;
+    int length = str.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] != ',' && str[i] != ' ') sum = sum + str[i];                             // Проверка на не равенство запятой и пробелу. Сложение символов в строку.
+        else if (str[i] == ' ') ;                                                             // Если пробел - ничего не делаем
+        else if (str[i] == ',')                                                              // Если запятая, то конец числа. Преобразуем число из строки и проверяем его
+        {
+            number = Convert.ToInt32(sum);
+            if (number > 0)
+            {
+                count++;
+                sum = string.Empty;
+            }
+            else sum = string.Empty;
+        }
+    }
+    number = Convert.ToInt32(sum);                                                      // Последнее число не отделяется запятой
+    if (number > 0) count++;
+    Console.Write($"В наборе чисел [{str}] , кол-во чисел больше ноля: {count}");
+}
+
+string numbers = NumberEntry("Введите произвольный набор чисел через запятую: ");
+CountNumbersOverZero(numbers);
---
+++ task_43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+
+double NumberEntry(string str)                                             // Метод для ввода числа
+{
+    Console.Write(str);
+
+    double number = Convert.ToDouble(Console.ReadLine());
+
+    return number;
+}
+
+void CountMidpoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.Write($"Для уравнений y = k1 * x + b1 и y = k2 * x + b2, точкой пересечения является: ({x}, {y})");
+}
+
+double b1 = NumberEntry("Введите кофициент b1: ");
+double k1 = NumberEntry("Введите кофициент k1: ");
+double b2 = NumberEntry("Введите кофициент b2: ");
+double k2 = NumberEntry("Введите кофициент k2: ");
+CountMidpoint(b1, k1, b2, k2);
